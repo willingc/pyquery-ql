b5ee037838de02f7f88488cbf2d1f8bc4b87d227
Carol Willing <carolcode@willingconsulting.com>
2017-12-14 09:40:50 -0600

    minor refactor

--- pyquery-ql/pyquery-ql.py
+++ pyquery-ql/pyquery-ql.py
@@ -17,37 +17,60 @@ import pprint
 import requests
 
 
-# get api token and set authorization
-api_token = os.environ['GITHUB_API_TOKEN']
-headers = {'Authorization': f'token {api_token}'}
+def send_query(url, query, headers):
+    """Send a query.
 
-# set url to a graphql endpoint
-url = 'https://api.github.com/graphql'
+    With GraphQL and Python requests, a `post` is used when querying, instead
+    of a `get`. The `post` is used since the `query` must be sent to the
+    GraphQL endpoint. This differs from REST where the API endpoint has the
+    query in its url structure.
 
-# add a json query
-query = """
-{
-  organization(login: "jupyterhub") {
-    repositories(first: 26) {
-      totalCount
-      edges {
-        node {
-          name
-          url
-          issues(states: OPEN) {
-            totalCount
-          }
-          pullRequests(states: OPEN) {
-            totalCount
+    Returns:
+        result: query result
+
+    """
+
+    result = requests.post(url=url, json={'query': query}, headers=headers)
+    return result
+
+
+def print_query_response(response):
+    """print the query response"""
+    pprint.pprint(json.loads(response.text))
+
+
+if __name__ == '__main__':
+
+    # get api token and set authorization
+    api_token = os.environ['GITHUB_API_TOKEN']
+    headers = {'Authorization': f'token {api_token}'}
+
+    # set url to a GraphQL endpoint
+    url = 'https://api.github.com/graphql'
+
+    # add a json query
+    query = """
+    {
+      organization(login: "jupyterhub") {
+        repositories(first: 26) {
+          totalCount
+          edges {
+            node {
+              name
+              url
+              issues(states: OPEN) {
+                totalCount
+              }
+              pullRequests(states: OPEN) {
+                totalCount
+              }
+            }
           }
         }
       }
     }
-  }
-}
-"""
+    """
 
-# submit the request
-r = requests.post(url=url, json={'query': query}, headers=headers)
+    response = send_query(url, query, headers)
+    print_query_response(response)
 
-pprint.pprint(json.loads(r.text))
